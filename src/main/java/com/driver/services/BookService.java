26565c00aaffa232479872336ae9efcb89fa1390
SHARGINJAY PATEL <sharginjaypatel12021999@gmail.com>
2023-01-13 12:46:19 +0530

    Unique Commit

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -1,6 +1,8 @@
 package com.driver.services;
 
+import com.driver.models.Author;
 import com.driver.models.Book;
+import com.driver.repositories.AuthorRepository;
 import com.driver.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,12 +16,44 @@ public class BookService {
     @Autowired
     BookRepository bookRepository2;
 
-    public void createBook(Book book){
-        bookRepository2.save(book);
+    @Autowired
+    AuthorRepository authorRepository1;
+
+    public void createBook(Book book)
+    {
+
+        int authorId = book.getAuthor().getId();
+
+        Author author =  authorRepository1.findById(authorId).get();
+
+        author.getBooksWritten().add(book);
+
+        book.setAuthor(author);
+
+//      bookRepository2.save(book);  //bidirectional
+
+        authorRepository1.save(author);
+
     }
 
     public List<Book> getBooks(String genre, boolean available, String author){
-        List<Book> books = null; //find the elements of the list by yourself
-        return books;
+
+
+        if(genre != null && author != null)
+        {
+            return bookRepository2.findBooksByGenreAuthor(genre, author, available);
+        }
+        else if(genre != null)
+        {
+            return bookRepository2.findBooksByGenre(genre, available);
+        }
+        else if(author != null)
+        {
+            return bookRepository2.findBooksByAuthor(author, available);
+        }
+        else
+        {
+            return bookRepository2.findByAvailability(available);
+        }
     }
 }
